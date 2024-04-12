943efae7a0cc66bdb488ff76954f4eebcef54d20
xsx <825657193@qq.com>
2024-04-12 18:39:59 +0800

    fix: 图片出现在头部的bug

--- im-uniapp/store/chatStore.js
+++ im-uniapp/store/chatStore.js
@@ -178,11 +178,14 @@ export default {
 			}
 			// 根据id顺序插入，防止消息乱序
 			let insertPos = chat.messages.length;
-			for (let idx in chat.messages) {
-				if (chat.messages[idx].id && msgInfo.id < chat.messages[idx].id) {
-					insertPos = idx;
-					console.log(`消息出现乱序,位置:${chat.messages.length},修正至:${insertPos}`);
-					break;
+			// 防止 图片、文件 在发送方 显示 在顶端  因为还没存库，id=0
+			if(msgInfo.id && msgInfo.id > 0){
+				for (let idx in chat.messages) {
+					if (chat.messages[idx].id && msgInfo.id < chat.messages[idx].id) {
+						insertPos = idx;
+						console.log(`消息出现乱序,位置:${chat.messages.length},修正至:${insertPos}`);
+						break;
+					}
 				}
 			}
 			chat.messages.splice(insertPos, 0, msgInfo);
