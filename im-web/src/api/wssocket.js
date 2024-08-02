cb8dbecb1a0c33d304297593504eedf3b537ef6a
xsx <825657193@qq.com>
2024-08-03 00:42:32 +0800

    im-ui更名为im-web

---
+++ im-web/src/api/wssocket.js
@@ -0,0 +1,145 @@
+var websock = null;
+let rec; //断线重连后，延迟5秒重新创建WebSocket连接  rec用来存储延迟请求的代码
+let isConnect = false; //连接标识 避免重复连接
+let connectCallBack= null;
+let messageCallBack = null;
+let closeCallBack = null
+
+
+let connect = (wsurl,accessToken) => {
+	try {
+		if (isConnect) {
+			return;
+		}
+		console.log("连接WebSocket");
+		websock = new WebSocket(wsurl); 
+		websock.onmessage = function(e) {
+			let sendInfo = JSON.parse(e.data)
+			if (sendInfo.cmd == 0) {	
+				heartCheck.start()
+				// 登录成功才算真正完成连接
+				connectCallBack && connectCallBack();
+				console.log('WebSocket登录成功')
+			} else if (sendInfo.cmd == 1) {
+				// 重新开启心跳定时
+				heartCheck.reset();
+			} else {
+				// 其他消息转发出去
+				console.log("收到消息:",sendInfo);  
+				messageCallBack && messageCallBack(sendInfo.cmd, sendInfo.data)
+			}
+		}
+		websock.onclose = function(e) {
+			console.log('WebSocket连接关闭')
+			isConnect = false; //断开后修改标识
+			closeCallBack && closeCallBack(e);
+		}
+		websock.onopen = function() {
+			console.log("WebSocket连接成功");
+			isConnect = true;
+			// 发送登录命令
+			let loginInfo = {
+				cmd: 0,
+				data: {
+					accessToken: accessToken
+				}
+			};
+			websock.send(JSON.stringify(loginInfo));
+		}
+
+		// 连接发生错误的回调方法
+		websock.onerror = function() {
+			console.log('WebSocket连接发生错误')
+			isConnect = false; //连接断开修改标识
+			reconnect(wsurl,accessToken);
+		}
+	} catch (e) {
+		console.log("尝试创建连接失败");
+		reconnect(wsurl,accessToken); //如果无法连接上webSocket 那么重新连接！可能会因为服务器重新部署，或者短暂断网等导致无法创建连接
+	}
+};
+
+//定义重连函数
+let reconnect = (wsurl,accessToken) => {
+	console.log("尝试重新连接");
+	if (isConnect){
+		//如果已经连上就不在重连了
+		return; 
+	}
+	rec && clearTimeout(rec);
+	rec = setTimeout(function() { // 延迟5秒重连  避免过多次过频繁请求重连
+		connect(wsurl,accessToken);
+	}, 15000);
+};
+//设置关闭连接
+let close = (code) => {
+	websock && websock.close(code);
+};
+
+
+//心跳设置
+let heartCheck = {
+	timeout: 5000, //每段时间发送一次心跳包 这里设置为20s
+	timeoutObj: null, //延时发送消息对象（启动心跳新建这个对象，收到消息后重置对象）
+	start: function() {
+		if (isConnect) {
+			console.log('发送WebSocket心跳')
+			let heartBeat = {
+				cmd: 1,
+				data: {}
+			};
+			websock.send(JSON.stringify(heartBeat))
+		}
+	},
+
+	reset: function() {
+		clearTimeout(this.timeoutObj);
+		this.timeoutObj = setTimeout(function() {
+			heartCheck.start();
+		}, this.timeout);
+
+	}
+};
+
+
+
+// 实际调用的方法
+let sendMessage = (agentData) => {
+	if (websock.readyState === websock.OPEN) {
+		// 若是ws开启状态
+		websock.send(JSON.stringify(agentData))
+	} else if (websock.readyState === websock.CONNECTING) {
+		// 若是 正在开启状态，则等待1s后重新调用
+		setTimeout(function() {
+			sendMessage(agentData)
+		}, 1000)
+	} else {
+		// 若未开启 ，则等待1s后重新调用
+		setTimeout(function() {
+			sendMessage(agentData)
+		}, 1000)
+	}
+}
+
+let onConnect = (callback) => {
+	connectCallBack = callback;
+}
+
+let onMessage = (callback) => {
+	messageCallBack = callback;
+}
+
+
+let onClose = (callback) => {
+	closeCallBack = callback;
+}
+// 将方法暴露出去
+export {
+	connect,
+	reconnect,
+	close,
+	sendMessage,
+	onConnect,
+	onMessage,
+	onClose,
+}
---
+++ im-web/src/view/Register.vue
@@ -0,0 +1,162 @@
+<template>
+	<el-container class="register-view">
+		<div>
+			<el-form :model="registerForm" status-icon :rules="rules" ref="registerForm" label-width="80px" class="web-ruleForm">
+				<div class="register-brand">欢迎成为盒子IM的用户</div>
+				<el-form-item label="用户名" prop="userName">
+					<el-input type="userName" v-model="registerForm.userName" autocomplete="off" placeholder="用户名(登录使用)"></el-input>
+				</el-form-item>
+				<el-form-item label="昵称" prop="nickName">
+					<el-input type="nickName" v-model="registerForm.nickName" autocomplete="off" placeholder="昵称"></el-input>
+				</el-form-item>
+				<el-form-item label="密码" prop="password">
+					<el-input type="password" v-model="registerForm.password" autocomplete="off" placeholder="密码"></el-input>
+				</el-form-item>
+				<el-form-item label="确认密码" prop="confirmPassword">
+					<el-input type="password" v-model="registerForm.confirmPassword" autocomplete="off" placeholder="确认密码"></el-input>
+				</el-form-item>
+				<el-form-item>
+					<el-button type="primary" @click="submitForm('registerForm')">注册</el-button>
+					<el-button @click="resetForm('registerForm')">清空</el-button>
+				</el-form-item>
+				<div class="to-login">
+					<router-link to="/login">已有账号,前往登录</router-link>
+				</div>
+			</el-form>
+		</div>
+		<icp></icp>
+	</el-container>
+</template>
+
+<script>
+	import Icp from '../components/common/Icp.vue'
+	export default {
+		name: "login",
+		components: {
+			Icp
+		},
+		data() {
+			var checkUserName = (rule, value, callback) => {
+				if (!value) {
+					return callback(new Error('请输入用户名'));
+				}
+				callback();
+			};
+			var checkNickName = (rule, value, callback) => {
+				if (!value) {
+					return callback(new Error('请输入昵称'));
+				}
+				callback();
+			};
+			var checkPassword = (rule, value, callback) => {
+				if (value === '') {
+					return callback(new Error('请输入密码'));
+				}
+				callback();
+			};
+
+			var checkConfirmPassword = (rule, value, callback) => {
+				if (value === '') {
+					return callback(new Error('请输入密码'));
+				}
+				if (value != this.registerForm.password) {
+					return callback(new Error('两次密码输入不一致'));
+				}
+				callback();
+			};
+
+
+			return {
+				registerForm: {
+					userName: '',
+					nickName: '',
+					password: '',
+					confirmPassword: ''
+				},
+				rules: {
+					userName: [{
+						validator: checkUserName,
+						trigger: 'blur'
+					}],
+					nickName: [{
+						validator: checkNickName,
+						trigger: 'blur'
+					}],
+					password: [{
+						validator: checkPassword,
+						trigger: 'blur'
+					}],
+					confirmPassword: [{
+						validator: checkConfirmPassword,
+						trigger: 'blur'
+					}]
+				}
+			};
+		},
+		methods: {
+			submitForm(formName) {
+				this.$refs[formName].validate((valid) => {
+					if (valid) {
+						this.$http({
+								url: "/register",
+								method: 'post',
+								data: this.registerForm
+							})
+							.then((data) => {
+								this.$message.success("注册成功!");
+							})
+					}
+				});
+			},
+			resetForm(formName) {
+				this.$refs[formName].resetFields();
+			}
+		}
+	}
+</script>
+
+<style scoped lang="scss">
+	.register-view {
+		position: fixed;
+		display: flex;
+		justify-content: space-around;
+		align-items: center;
+		width: 100%;
+		height: 100%;
+		background: rgb(232, 242, 255);
+		
+		.web-ruleForm {
+			width: 500px;
+			height: 450px;
+			padding: 20px;
+			background: white;
+			opacity: 0.9;
+			box-shadow: 0px 0px  1px #ccc;
+			border-radius: 3px;
+			overflow: hidden;
+			border-radius: 3%;
+			
+			.register-brand {
+				line-height: 50px;
+				margin: 20px 0 30px 0;
+				font-size: 22px;
+				font-weight: 600;
+				letter-spacing: 2px;
+				text-align: center;
+				text-transform: uppercase;
+			}
+			
+			.to-login {
+				display: flex;
+				flex-direction: row-reverse;
+				line-height: 40px;
+				text-align: left;
+				padding-left: 20px;
+			}
+		}
+	}
+
+	
+
+	
+</style>
