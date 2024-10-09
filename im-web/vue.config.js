633dc3292ef53ebcc2528fa1feff8e1a97aa1e9e
xsx <825657193@qq.com>
2024-10-09 18:06:59 +0800

    移除开发环境ssl证书，避免造成歧义

--- im-web/vue.config.js
+++ im-web/vue.config.js
@@ -12,12 +12,7 @@ module.exports = {
 					'^/api': ''
 				}
 			}
-		},
-		// 音视频功能需要ssl证书，如需调试请打开注释
-		// https: {
-		// 	cert: fs.readFileSync(path.join(__dirname, 'src/ssl/cert.crt')),
-		// 	key: fs.readFileSync(path.join(__dirname, 'src/ssl/cert.key'))
-		// }
+		}
 	}
 
 }
